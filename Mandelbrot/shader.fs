0aca21a558f8799cc88bea40067d1b6f1f051b71
feive7 <feive7@gmail.com>
2025-11-26 21:33:01 -0800

    distance gradient

--- Mandelbrot/shader.fs
+++ Mandelbrot/shader.fs
@@ -11,18 +11,17 @@ vec2 complex_square(vec2 z) {
     return vec2(z.x*z.x-z.y*z.y,2*z.x*z.y);
 }
 
-int mandelbrot(vec2 c) {
+float mandelbrot(vec2 c) {
     vec2 z = c;
     int i;
     for(i = 0; i < iterations; i++) {
         if(length(z) > 4) break;
         z = complex_square(z) + c;
     }
-    if(i<iterations) return i; // Point is in the mandelbrot set
-    return 0; // Point is not in the mandelbrot set
+    return distance(z,c);
 }
 
 void main() {
-    float value = sqrt(mandelbrot(fragTexCoord) / iterations);
+    float value = sqrt(mandelbrot(fragTexCoord));
     finalColor = vec4(value,value,value,1.0);
 }
