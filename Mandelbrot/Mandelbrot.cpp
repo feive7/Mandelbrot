21195e07b75951f3e1b178eb84c91f92ea2b6da5
feive7 <feive7@gmail.com>
2025-11-26 19:18:19 -0800

    Hello Raylib

--- Mandelbrot/Mandelbrot.cpp
+++ Mandelbrot/Mandelbrot.cpp
@@ -1,5 +1,25 @@
 #include <iostream>
+#include <raylib.h>
 
 int main() {
-    std::cout << "Hello World!\n";
+	// Define window dimensions
+	const int window_width = 400;
+	const int window_height = 400;
+
+	// Create window and GL context
+	InitWindow(window_width, window_height, "Raylib");
+	SetTargetFPS(60);
+
+	// Game loop
+	while (!WindowShouldClose()) {
+		BeginDrawing();
+		ClearBackground(RAYWHITE);
+		DrawText("Hello World!", 5, 5, 20, BLACK);
+		EndDrawing();
+	}
+
+	// Destroy window and GL context
+	CloseWindow();
+
+	return 0;
 }
