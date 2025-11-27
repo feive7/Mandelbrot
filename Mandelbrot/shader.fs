650f2cea2d4f85fb3cf5eff8331f36c9a000ea09
feive7 <feive7@gmail.com>
2025-11-26 19:31:17 -0800

    mandelbrot shader

--- Mandelbrot/shader.fs
+++ Mandelbrot/shader.fs
@@ -6,6 +6,23 @@ in vec2 fragTexCoord;
 // Output color
 out vec4 finalColor;
 
+// Square complex number (x+yi)^2
+vec2 complex_square(vec2 z) {
+    return vec2(z.x*z.x-z.y*z.y,2*z.x*z.y);
+}
+
+int mandelbrot(vec2 c) {
+    vec2 z = c;
+    int i;
+    for(i = 0; i < 40; i++) {
+        if(length(z) > 4) break;
+        z = complex_square(z) + c;
+    }
+    if(i<40) return 1; // Point is in the mandelbrot set
+    return 0; // Point is not in the mandelbrot set
+}
+
 void main() {
-    finalColor = vec4(fragTexCoord,1.0,1.0);
+    float value = mandelbrot(fragTexCoord);
+    finalColor = vec4(value,value,value,1.0);
 }
